334729d5f82e7123631adb2e515d1d82316b892e
Walter Sandmeier <walter.sandmeier@gmail.com>
2024-09-08 17:47:13 -0400

    ESLint + Prettier Configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'cfbheadlines': ['"CFBHeadlines"', 'sans-serif'],
-        'cfbbody': ['"CFBBody"', 'sans-serif'],
-        'cfbsubheading': ['"CFBSubheading"', 'sans-serif'],
-      },
-      colors: {
-        'campus-yellow': '#ffff3c', 
-        'campus-black': '#252525',
-      },
-    }
-  },
-  plugins: [],
+    content: ['./*.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                cfbheadlines: ['"CFBHeadlines"', 'sans-serif'],
+                cfbbody: ['"CFBBody"', 'sans-serif'],
+                cfbsubheading: ['"CFBSubheading"', 'sans-serif'],
+            },
+            colors: {
+                'campus-yellow': '#ffff3c',
+                'campus-black': '#252525',
+            },
+        },
+    },
+    plugins: [],
 };
