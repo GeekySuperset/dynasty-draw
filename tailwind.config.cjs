b50a818b0e91506748a409152584d21ceb2f485c
Walter Sandmeier <walter.sandmeier@gmail.com>
2024-09-08 23:22:15 -0400

    Move project files to src/

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './main.js'],
+    content: ['./src/index.html', './src/main.js'],
     theme: {
         extend: {
             fontFamily: {
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('vite').UserConfig} */
 export default {
-    // ...
+    root: './src',
+    publicDir: '../public',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
 };
